9369a2b485e17b5e9e01509840979a6cf1f1a44a
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-05-12 13:08:26 +0200

    Added forkserver test program

---
+++ dragonfly/src/mutators/nop.rs
@@ -0,0 +1,29 @@
+use libafl::prelude::{
+    Error,
+    Mutator, MutationResult,
+    Input, Named,
+};
+
+pub struct NopMutator;
+
+impl NopMutator {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<I, S> Mutator<I, S> for NopMutator
+where
+    I: Input,
+{
+    fn mutate(&mut self, _state: &mut S, _input: &mut I, _stage_idx: i32) -> Result<MutationResult, Error> {
+        Ok(MutationResult::Mutated)
+    }
+}
+
+impl Named for NopMutator {
+    fn name(&self) -> &str {
+        "NopMutator"
+    }
+}
