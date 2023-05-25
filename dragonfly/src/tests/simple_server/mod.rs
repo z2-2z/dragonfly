e005ae1447f6ccb8c77996eadd3a1bd8d4c642b6
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-05-25 21:11:37 +0200

    Enabled multi-core fuzzing on simple_server

--- dragonfly/src/tests/simple_server/mod.rs
+++ dragonfly/src/tests/simple_server/mod.rs
@@ -7,16 +7,17 @@ use libafl::{
             ShMem,
             ShMemProvider,
             UnixShMemProvider,
+            StdShMemProvider,
         },
         tuples::tuple_list,
         AsMutSlice,
         HasLen,
+        core_affinity::{Cores, CoreId},
+        launcher::Launcher,
     },
     corpus::{
         InMemoryCorpus,
-        OnDiskCorpus,
     },
-    events::SimpleEventManager,
     feedback_or,
     feedbacks::{
         CrashFeedback,
@@ -46,6 +47,7 @@ use libafl::{
     state::StdState,
     Error,
     Evaluator,
+    events::EventConfig,
 };
 use nix::sys::signal::Signal;
 use serde::{
@@ -137,8 +139,6 @@ impl HasLen for ExampleInput {
 
 #[test]
 fn simple_server() {
-    affinity::set_thread_affinity([0]).unwrap();
-
     println!("Workdir: {:?}", std::env::current_dir().unwrap().to_string_lossy().to_string());
 
     // For fuzzbench, crashes and finds are inside the same `corpus` directory, in the "queue" and "crashes" subdir.
@@ -170,104 +170,111 @@ fn simple_server() {
 
 /// The actual fuzzer
 #[allow(clippy::too_many_arguments)]
-fn fuzz(objective_dir: PathBuf, logfile: &PathBuf, timeout: Duration, executable: &str, debug_child: bool, signal: Signal, arguments: &[String]) -> Result<(), Error> {
-    // a large initial map size that should be enough
-    // to house all potential coverage maps for our targets
-    // (we will eventually reduce the used size according to the actual map)
-    const MAP_SIZE: usize = 65536;
+fn fuzz(_objective_dir: PathBuf, logfile: &PathBuf, timeout: Duration, executable: &str, debug_child: bool, signal: Signal, arguments: &[String]) -> Result<(), Error> {
+    let mut client = |old_state: Option<_>, mut mgr, core: CoreId| {
+        println!("Launch client @ {}", core.0);
+        let mut shmem_provider = UnixShMemProvider::new()?;
+        
+        // The coverage map shared between observer and executor
+        const MAP_SIZE: usize = 65536;
+        let mut shmem = shmem_provider.new_shmem(MAP_SIZE)?;
+        // let the forkserver know the shmid
+        shmem.write_to_env("__AFL_SHM_ID")?;
+        let shmem_buf = shmem.as_mut_slice();
+        // To let know the AFL++ binary that we have a big map
+        std::env::set_var("AFL_MAP_SIZE", format!("{}", MAP_SIZE));
 
-    // 'While the monitor are state, they are usually used in the broker - which is likely never restarted
+        // Create an observation channel using the hitcounts map of AFL++
+        let edges_observer = HitcountsMapObserver::new(unsafe { StdMapObserver::new("shared_mem", shmem_buf) });
+
+        // Create an observation channel to keep track of the execution time
+        let time_observer = TimeObserver::new("time");
+
+        let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
+
+        let calibration = CalibrationStage::new(&map_feedback);
+
+        // Feedback to rate the interestingness of an input
+        // This one is composed by two Feedbacks in OR
+        let mut feedback = feedback_or!(
+            // New maximization map feedback linked to the edges observer and the feedback state
+            map_feedback,
+            // Time feedback, this one does not need a feedback state
+            TimeFeedback::with_observer(&time_observer)
+        );
+
+        // A feedback to choose if an input is a solution or not
+        let mut objective = CrashFeedback::new();
+
+        // create a State from scratch
+        let mut state = old_state.unwrap_or_else(|| StdState::new(
+            // RNG
+            StdRand::with_seed(current_nanos()),
+            // Corpus that will be evolved, we keep it in memory for performance
+            InMemoryCorpus::<ExampleInput>::new(),
+            // Corpus in which we store solutions (crashes in this example),
+            // on disk so the user can get them after stopping the fuzzer
+            InMemoryCorpus::new(),
+            // States of the feedbacks.
+            // The feedbacks can report the data that should persist in the State.
+            &mut feedback,
+            // Same for objective feedbacks
+            &mut objective,
+        ).unwrap());
+
+        let mutator = StdScheduledMutator::new(tuple_list!(PacketDeleteMutator::new(1), PacketDuplicateMutator::new(16), PacketReorderMutator::new()));
+
+        let power = StdMutationalStage::new(mutator);
+
+        let scheduler = RandScheduler::new();
+
+        let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+
+        let mut executor = DragonflyExecutorBuilder::new()
+            .observers(tuple_list!(edges_observer, time_observer))
+            .shmem_provider(&mut shmem_provider)
+            .timeout(timeout)
+            .signal(signal)
+            .debug_child(debug_child)
+            .program(executable)
+            .args(arguments)
+            .is_deferred_forkserver(true)
+            .env("LD_LIBRARY_PATH", "src/tests/simple_server/build")
+            .build()?;
+
+        // The order of the stages matter!
+        let mut stages = tuple_list!(calibration, power);
+
+        // evaluate input
+        let input = ExampleInput {
+            packets: vec![Packet::Add4, Packet::Sep, Packet::Sub1, Packet::Sep, Packet::NegS, Packet::Sep],
+        };
+        fuzzer.add_input(&mut state, &mut executor, &mut mgr, input)?;
+        
+        println!("Start fuzzing @ {}", core.0);
+
+        fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)?;
+        Ok(())
+    };
+
+    let cores = Cores::all()?;
     let monitor = OnDiskTOMLMonitor::new(logfile, SimplePrintingMonitor::new());
 
-    // The event manager handle the various events generated during the fuzzing loop
-    // such as the notification of the addition of a new item to the corpus
-    let mut mgr = SimpleEventManager::new(monitor);
+    let mut launcher = Launcher::builder()
+        .shmem_provider(StdShMemProvider::new()?)
+        .configuration(EventConfig::from_name("default"))
+        .run_client(&mut client)
+        .cores(&cores)
+        .monitor(monitor)
+        .broker_port(1337)
+        .remote_broker_addr(Some("127.0.0.1:1337".parse().unwrap()))
+        .build();
 
-    // The unix shmem provider for shared memory, to match AFL++'s shared memory at the target side
-    let mut shmem_provider = UnixShMemProvider::new().unwrap();
-
-    // The coverage map shared between observer and executor
-    let mut shmem = shmem_provider.new_shmem(MAP_SIZE).unwrap();
-    // let the forkserver know the shmid
-    shmem.write_to_env("__AFL_SHM_ID").unwrap();
-    let shmem_buf = shmem.as_mut_slice();
-    // To let know the AFL++ binary that we have a big map
-    std::env::set_var("AFL_MAP_SIZE", format!("{}", MAP_SIZE));
-
-    // Create an observation channel using the hitcounts map of AFL++
-    let edges_observer = HitcountsMapObserver::new(unsafe { StdMapObserver::new("shared_mem", shmem_buf) });
-
-    // Create an observation channel to keep track of the execution time
-    let time_observer = TimeObserver::new("time");
-
-    let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
-
-    let calibration = CalibrationStage::new(&map_feedback);
-
-    // Feedback to rate the interestingness of an input
-    // This one is composed by two Feedbacks in OR
-    let mut feedback = feedback_or!(
-        // New maximization map feedback linked to the edges observer and the feedback state
-        map_feedback,
-        // Time feedback, this one does not need a feedback state
-        TimeFeedback::with_observer(&time_observer)
-    );
-
-    // A feedback to choose if an input is a solution or not
-    let mut objective = CrashFeedback::new();
-
-    // create a State from scratch
-    let mut state = StdState::new(
-        // RNG
-        StdRand::with_seed(current_nanos()),
-        // Corpus that will be evolved, we keep it in memory for performance
-        InMemoryCorpus::<ExampleInput>::new(),
-        // Corpus in which we store solutions (crashes in this example),
-        // on disk so the user can get them after stopping the fuzzer
-        OnDiskCorpus::new(objective_dir).unwrap(),
-        // States of the feedbacks.
-        // The feedbacks can report the data that should persist in the State.
-        &mut feedback,
-        // Same for objective feedbacks
-        &mut objective,
-    )
-    .unwrap();
-
-    // Setup a MOPT mutator
-    //let mutator = StdMOptMutator::new(&mut state, tuple_list!(NopMutator::new()), 7, 5)?;
-    let mutator = StdScheduledMutator::new(tuple_list!(PacketDeleteMutator::new(1), PacketDuplicateMutator::new(16), PacketReorderMutator::new()));
-
-    let power = StdMutationalStage::new(mutator);
-
-    // A minimization+queue policy to get testcasess from the corpus
-    let scheduler = RandScheduler::new();
-
-    // A fuzzer with feedbacks and a corpus scheduler
-    let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
-
-    let mut executor = DragonflyExecutorBuilder::new()
-        .observers(tuple_list!(edges_observer, time_observer))
-        .shmem_provider(&mut shmem_provider)
-        .timeout(timeout)
-        .signal(signal)
-        .debug_child(debug_child)
-        .program(executable)
-        .args(arguments)
-        .is_deferred_forkserver(true)
-        .env("LD_LIBRARY_PATH", "src/tests/simple_server/build")
-        .build()?;
-
-    // The order of the stages matter!
-    let mut stages = tuple_list!(calibration, power);
-
-    // evaluate input
-    let input = ExampleInput {
-        packets: vec![Packet::Add4, Packet::Sep, Packet::Sub1, Packet::Sep, Packet::NegS, Packet::Sep],
+    match launcher.launch() {
+        Ok(_) => {},
+        Err(Error::ShuttingDown) => {},
+        err => panic!("Failed to lauch instances: {:?}", err)
     };
-    fuzzer.add_input(&mut state, &mut executor, &mut mgr, input)?;
-
-    //fuzzer.fuzz_loop_for(&mut stages, &mut executor, &mut state, &mut mgr, 1)?;
-    fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)?;
 
     Ok(())
 }
