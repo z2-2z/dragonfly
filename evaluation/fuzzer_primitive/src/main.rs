d32d730b68f06dae675ec534a38e7ce4c6952c9b
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-05-30 11:33:12 +0200

    Cleaned up fuzzer_primitive

--- evaluation/fuzzer_primitive/src/main.rs
+++ evaluation/fuzzer_primitive/src/main.rs
@@ -8,7 +8,8 @@ use libafl::prelude::{
     HasLen,
     Cores, CoreId,
     Launcher,
-    InMemoryCorpus,
+    CachedOnDiskCorpus,
+    OnDiskCorpus,
     feedback_or,
     CrashFeedback,
     MaxMapFeedback,
@@ -115,28 +116,26 @@ impl HasLen for FTPInput {
     }
 }
 
-fn main() {
-    // For fuzzbench, crashes and finds are inside the same `corpus` directory, in the "queue" and "crashes" subdir.
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = std::env::args().collect();
+    
+    let cores = args.get(1).map(|x| x.as_str()).unwrap_or("0");
+    
     let out_dir = PathBuf::from("output");
     let _ = fs::create_dir(&out_dir);
-    if !out_dir.is_dir() {
-        println!("Out dir at {:?} is not a valid directory!", &out_dir);
-        return;
-    }
-
-    let mut crashes = out_dir;
+    
+    let mut crashes = out_dir.clone();
     crashes.push("crashes");
-
-    let logfile = PathBuf::from("output/log");
-
+    
+    let mut queue = out_dir.clone();
+    queue.push("queue");
+    
+    let mut logfile = out_dir;
+    logfile.push("log");
+    
     let timeout = Duration::from_millis(5000);
-
+    
     let executable = "../proftpd/proftpd".to_string();
-
-    let debug_child = true;
-
-    let signal = str::parse::<Signal>("SIGKILL").unwrap();
-
     let arguments = vec![
         "-d".to_string(),
         "0".to_string(),
@@ -146,66 +145,45 @@ fn main() {
         fs::canonicalize("../fuzz.conf").unwrap().to_str().unwrap().to_string(),
         "-n".to_string(),
     ];
-
-    fuzz(crashes, &logfile, timeout, &executable, debug_child, signal, &arguments).expect("An error occurred while fuzzing");
-}
-
-/// The actual fuzzer
-#[allow(clippy::too_many_arguments)]
-fn fuzz(objective_dir: PathBuf, logfile: &PathBuf, timeout: Duration, executable: &str, debug_child: bool, signal: Signal, arguments: &[String]) -> Result<(), Error> {
+    
+    let debug_child = true;
+    
+    let signal = str::parse::<Signal>("SIGKILL").unwrap();
+    
+    let seed = current_nanos();
+    
     let mut client = |old_state: Option<_>, mut mgr, core: CoreId| {
         println!("Launch client @ {}", core.0);
-        let mut shmem_provider = UnixShMemProvider::new()?;
         
-        // The coverage map shared between observer and executor
+        let mut shmem_provider = UnixShMemProvider::new()?;
         const MAP_SIZE: usize = 65536;
         let mut shmem = shmem_provider.new_shmem(MAP_SIZE)?;
-        // let the forkserver know the shmid
         shmem.write_to_env("__AFL_SHM_ID")?;
         let shmem_buf = shmem.as_mut_slice();
-        // To let know the AFL++ binary that we have a big map
         std::env::set_var("AFL_MAP_SIZE", format!("{}", MAP_SIZE));
 
         let state_observer = StateObserver::new(&mut shmem_provider, "StateObserver")?;
-
-        // Create an observation channel using the hitcounts map of AFL++
         let edges_observer = HitcountsMapObserver::new(unsafe { StdMapObserver::new("shared_mem", shmem_buf) });
-
-        // Create an observation channel to keep track of the execution time
         let time_observer = TimeObserver::new("time");
         
         let state_feedback = StateFeedback::new(&state_observer);
-
         let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
 
         let calibration = CalibrationStage::new(&map_feedback);
 
-        // Feedback to rate the interestingness of an input
-        // This one is composed by two Feedbacks in OR
         let mut feedback = feedback_or!(
             state_feedback,
-            // New maximization map feedback linked to the edges observer and the feedback state
             map_feedback,
-            // Time feedback, this one does not need a feedback state
             TimeFeedback::with_observer(&time_observer)
         );
 
-        // A feedback to choose if an input is a solution or not
         let mut objective = CrashFeedback::new();
 
-        // create a State from scratch
         let mut state = old_state.unwrap_or_else(|| StdState::new(
-            // RNG
-            StdRand::with_seed(current_nanos()),
-            // Corpus that will be evolved, we keep it in memory for performance
-            InMemoryCorpus::<FTPInput>::new(),
-            // Corpus in which we store solutions (crashes in this example),
-            // on disk so the user can get them after stopping the fuzzer
-            InMemoryCorpus::new(),
-            // States of the feedbacks.
-            // The feedbacks can report the data that should persist in the State.
+            StdRand::with_seed(seed),
+            CachedOnDiskCorpus::<FTPInput>::new(&queue, 128).expect("queue"),
+            OnDiskCorpus::<FTPInput>::new(&crashes).expect("crashes"),
             &mut feedback,
-            // Same for objective feedbacks
             &mut objective,
         ).unwrap());
         state.init_stategraph();
@@ -232,18 +210,16 @@ fn fuzz(objective_dir: PathBuf, logfile: &PathBuf, timeout: Duration, executable
             .timeout(timeout)
             .signal(signal)
             .debug_child(debug_child)
-            .program(executable)
-            .args(arguments)
+            .program(&executable)
+            .args(&arguments)
             .is_deferred_forkserver(true)
             .build()?;
 
-        // The order of the stages matter!
         let mut stages = tuple_list!(
             calibration, 
             mutational
         );
 
-        // evaluate input
         let input = FTPInput {
             packets: vec![
                 FTPPacket::Ctrl(BytesInput::new(b"USER ftp\r\n".to_vec())),
@@ -263,11 +239,13 @@ fn fuzz(objective_dir: PathBuf, logfile: &PathBuf, timeout: Duration, executable
         };
         fuzzer.evaluate_input(&mut state, &mut executor, &mut mgr, input)?;
 
-        fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)?;
+        fuzzer.fuzz_loop_for(&mut stages, &mut executor, &mut state, &mut mgr, 1)?;
+        
+        println!("Stopping client {}", core.0);
         Ok(())
     };
 
-    let cores = Cores::from_cmdline("0")?;
+    let cores = Cores::from_cmdline(cores)?;
     let monitor = OnDiskTOMLMonitor::new(logfile, SimplePrintingMonitor::new());
 
     let mut launcher = Launcher::builder()
