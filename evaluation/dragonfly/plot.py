259db67d4d275aaae4c1cbe8e00f865e6ab46011
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-06-14 22:09:26 +0200

    Supporting multiple plots in plotting script

--- evaluation/dragonfly/plot.py
+++ evaluation/dragonfly/plot.py
@@ -25,24 +25,26 @@ def extract(data, key):
     return x, y, xlabel, ylabel
 
 def main():
-    logfile = sys.argv[1]
-    
-    data = []
-    with open(logfile) as f:
-        for line in f:
-            line = json.loads(line.strip())
-            data.append(line)
-    
-    #x, y, xlabel, ylabel = plot_data = extract(data, "exec_sec")
-    x, y, xlabel, ylabel = extract(data, "corpus")
-    
     fig, ax = plt.subplots()
-    ax.plot(x, y)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_title("dragonfly fuzzer")
-    ax.grid(True)
-    #ax.set_xscale("log")
+    
+    for logfile in sys.argv[1:]:
+        data = []
+        with open(logfile) as f:
+            for line in f:
+                line = json.loads(line.strip())
+                data.append(line)
+        
+        #x, y, xlabel, ylabel = plot_data = extract(data, "exec_sec")
+        x, y, xlabel, ylabel = extract(data, "corpus")
+        
+        ax.plot(x, y, label=logfile)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_title("dragonfly fuzzer")
+        ax.grid(True)
+        ax.legend()
+        #ax.set_xscale("log")
+    
     plt.show()
 
 if __name__ == "__main__":
