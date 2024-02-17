0144d2dde614df890a49a803f356044fcc1be165
z2_ <88509734+z2-2z@users.noreply.github.com>
2024-02-17 05:11:26 +0100

    Improved test harness

--- libdragonfly/tests/packet_channel_bytewise.c
+++ libdragonfly/tests/packet_channel_bytewise.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "packet_channel.h"
 
 char packet_buf[16 * 1024 * 1024];
 
-int main (void) {
+int main (int argc, char** argv) {
+    size_t amount = 1;
+    
+    if (argc > 1) {
+        amount = atoi(argv[1]);
+    }
+    
     if (read(0, packet_buf, sizeof(packet_buf)) < 0) {
         return 1;
     }
@@ -12,34 +19,32 @@ int main (void) {
     packet_channel_init(packet_buf);
     
     while (1) {
-        char c = 0;
+        char buf[amount + 1];
         
         packet_channel_check_available_data();
         int next_0 = packet_channel_has_data(0);
         int next_1 = packet_channel_has_data(1);
         
         if (next_0) {
-            size_t ret = packet_channel_read(0, &c, 1);
+            size_t ret = packet_channel_read(0, buf, amount);
             
             if (ret == 0) {
                 printf("(EOF 0) ");
                 break;
-            } else if (ret == 1) {
-                printf("(0) %c ", c);
             } else {
-                return 1;
+                buf[ret] = 0;
+                printf("(0) \"%s\" ", buf);
             }
         }
         
         if (next_1) {
-            size_t ret = packet_channel_read(1, &c, 1);
+            size_t ret = packet_channel_read(1, buf, amount);
             
             if (ret == 0) {
                 printf("(EOF 1) ");
-            } else if (ret == 1) {
-                printf("(1) %c ", c);
             } else {
-                return 1;
+                buf[ret] = 0;
+                printf("(1) \"%s\" ", buf);
             }
         }
         
