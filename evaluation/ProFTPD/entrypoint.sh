9b373316568fbe699df8815457aac55069122c2e
z2_ <88509734+z2-2z@users.noreply.github.com>
2024-03-14 03:30:21 +0100

    Docker setup working

--- evaluation/ProFTPD/entrypoint.sh
+++ evaluation/ProFTPD/entrypoint.sh
@@ -3,4 +3,4 @@ set -ex;
 test -d /ftproot;
 test -d /output;
 chmod -R 777 /output;
-nohup timeout 24h ./fuzzer fuzz --output /output > /output/output &
+timeout 24h ./fuzzer fuzz --output /output $* &> /output/output
