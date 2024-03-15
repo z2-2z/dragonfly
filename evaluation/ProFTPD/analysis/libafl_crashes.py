d5c308938080d9e0faf65c3c1a12faca34ac9879
z2_ <88509734+z2-2z@users.noreply.github.com>
2024-03-15 06:59:16 +0100

    Added crash plotting script

---
+++ evaluation/ProFTPD/analysis/libafl_crashes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import matplotlib.pyplot as plt
+
+def main(logfiles):
+    fig, ax = plt.subplots()
+    
+    for logfile in logfiles:
+        num_clients = 0
+        x = []
+        y = []
+        
+        with open(logfile) as f:
+            for line in f:
+                data = json.loads(line.strip())
+                x.append(data["run_time"]["secs"])
+                y.append(data["objectives"])
+                num_clients = max(len(data["clients"]), num_clients)
+        
+        ax.plot(x, y, label=f"{logfile} ({num_clients} cores)")
+    
+    ax.set_xlabel("time")
+    ax.set_ylabel("objectives")
+    ax.set_title("dragonfly fuzzers")
+    ax.grid(True)
+    ax.legend()
+    
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
